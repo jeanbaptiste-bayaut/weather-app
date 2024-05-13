d8e803ed5d08ebfd7b2585a3244f859c4a2106d1
jeanbaptiste-bayaut <jean-baptiste.bayaut@oclock.school>
2024-05-13 20:29:59 +0200

    feat: :sparkles: feat hourly forecast

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,5 +1,6 @@
 const currentWeather = require('../data/currentWeatherData');
 const weatherIcons = require('../data/weatherIcons')
+const countryCodes = require('../data/countryCodes')
 
 const mainController = {
 
@@ -8,17 +9,28 @@ const mainController = {
     },
 
     async showWeather(req, res) {
-        const { city } = req.body
+        const { city, country_code } = req.body
         const cityWeather = await currentWeather.getCurrentWeather(city)
+        const cityLatLong = await currentWeather.getLatLongfromCountryCode(city, country_code)
+
+        const lat = cityLatLong[0].lat
+        const lon = cityLatLong[0].lon
+        const allWeather = await currentWeather.getAllWeather(lat, lon)
+
+        console.log(allWeather.hourly);
 
         const weatherIcon = cityWeather.weather[0].main.toLowerCase()
+
+        res.locals.weatherIcons = weatherIcons
 
         const icon = weatherIcons.find(elt => elt.type.toLowerCase() === weatherIcon)
         console.log(cityWeather);
 
         res.render('city', {
             cityWeather,
-            icon
+            icon,
+            countries: countryCodes,
+            allWeather
         })
     }
 }
--- app/data/currentWeatherData.js
+++ app/data/currentWeatherData.js
@@ -9,7 +9,32 @@ const currentWeather = {
         const weather = await response.json()
 
         return weather
-    }
+    },
+
+    async getLatLongfromCountryCode(city, countryCode) {
+        const apikey = '4e91f79aa0da851077b37cc931e6f94a'
+        const apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q='
+
+        const response = await fetch(apiUrl + city + ',' + countryCode + `&appid=${apikey}`)
+
+        const cityLatLong = await response.json()
+
+        return cityLatLong
+
+    },
+
+    async getAllWeather(lat, lon) {
+        const apikey = '4e91f79aa0da851077b37cc931e6f94a'
+        const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`
+
+        const response = await fetch(apiUrl);
+        const weatherData = await response.json();
+
+        return weatherData
+    },
+
+
+    
 
 }
 
